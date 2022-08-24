afe1406393b8072e8ad68991d8aa761e93e87920
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-24 05:16:45 -0400

    start with all components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <div className='w-full fle py-6 just justify-between items-center navbar'>
+
+    </div>
+  )
+}
+
+export default Navbar
