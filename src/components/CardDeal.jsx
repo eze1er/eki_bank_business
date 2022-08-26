c02c53360929dc01e4d8ef621205f02e29fc2cd4
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-26 04:05:49 -0400

    testimonials done

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -11,10 +11,10 @@ const CardDeal = () => (
         in few easy steps.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
-    <Button styles="mt-10" />
+    <Button styles={`mt-10`} />
     </div>
     <div className={layout.sectionImg}>
-      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
 
     </div>
   </section>
